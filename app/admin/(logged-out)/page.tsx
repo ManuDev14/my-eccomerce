c1a1cc7410a73c012965d568027e46d9b1a46570
Emanuel Vaca <emanuelvacaibanez@gmail.com>
2025-11-07 15:52:45 -0400

    change title in init page

--- app/admin/(logged-out)/page.tsx
+++ app/admin/(logged-out)/page.tsx
@@ -6,9 +6,9 @@ export default function LandingPage() {
   return (
     <>
       <h1 className="flex gap-2 items-center">
-        <PersonStandingIcon size={50} className="text-primary" /> SupportMe
+        <PersonStandingIcon size={50} className="text-primary" /> Eccomerce
       </h1>
-      <p>Panel administrativo para gestión de soporte</p>
+      <p>Panel administrativo de Mi Eccomerce</p>
       <div className="flex gap-2 items-center">
         <Button asChild>
           <Link href="/admin/login">Iniciar Sesión</Link>
